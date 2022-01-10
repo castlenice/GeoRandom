c7464391464b19c68a44b8b82976dc315796ba95
castlenice <sonja.schoenburg@gmail.com>
2022-01-10 10:47:50 +0100

    Hallo Jonas

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Header from './components/Header'
+
+
+function App() {
+  return (
+    <div className='container'>
+      <Header title={1} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const button = ({ color, text}) => {
+    return <button style={{ backgroundColor: color}} className='btn'>{text}</button>
+    
+}
+
+export default button
+
