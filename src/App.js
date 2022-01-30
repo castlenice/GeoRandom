7f9643cca28e639f5b6eb0554ad5b67ed4ac9af2
castlenice <sonja.schoenburg@gmail.com>
2022-01-30 14:13:38 +0100

    display map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React from "react";
+import GoogleMap, { Marker } from "react-maps-google";
+import "./App.css";
 
-import Header from './components/Header'
+// reactstrap components
+import {
+  Button
+ 
+} from "reactstrap";
+
+// core components
 
 
-function App() {
+const App = () => {
   return (
-    <div className='container'>
-      <Header title={1} />
+  <div className="App">
+    <div className="Map">
+      <GoogleMap apiKey="AIzaSyDN1EmofMqvJN0UTRO7noWW6ysjoscptEw">
+        <Marker position={{ lat: 40.7174343, lng: -73.9930319 }} />
+      </GoogleMap>
+    </div>
     </div>
   )
-}
+};
 
-export default App
+export default App;
