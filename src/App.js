7d0be38d6e866eed81a960bbe41db21419ac8e8e
castlenice <sonja.schoenburg@gmail.com>
2022-01-30 17:16:32 +0100

    make maprandom work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { LoadScript, GoogleMap } from "@react-google-maps/api";
+import { useState } from "react";
+import  randomLocation from 'random-location'
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // reactstrap components
 import { Button } from "reactstrap";
@@ -10,34 +13,47 @@ const containerStyle = {
   height: "600px",
 };
 
-const center = {
-  lat: 40.708831,
-  lng: -74.0166889,
-};
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
-
-const configureMap = (map) => {
-  //map.disableDefaultUI = true;
-  console.log(map);
+const randomCoordinates = () => {
+  const P = {
+    latitude: 51.453825, 
+    longitude: 7.032503
+  };
+  const R = 100000
+  const randomPoint = randomLocation.randomCirclePoint(P, R)
+  return {
+    lat: randomPoint.latitude ,
+    lng: randomPoint.longitude
+}
 };
 
 const App = () => {
+  const [location, setLocation] = useState(randomCoordinates());
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyDN1EmofMqvJN0UTRO7noWW6ysjoscptEw">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={15}
-        onLoad={configureMap}
-        options={{
-          disableDefaultUI: true,
-          mapTypeId: "satellite",
-          gestureHandling: "none",
-          tilt:45
-        }}
-    
-      ></GoogleMap>
-    </LoadScript>
+    <>
+      <LoadScript googleMapsApiKey="AIzaSyDN1EmofMqvJN0UTRO7noWW6ysjoscptEw">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={location}
+          zoom={15}
+          options={{
+            disableDefaultUI: true,
+            mapTypeId: "satellite",
+            gestureHandling: "none",
+            tilt: 45,
+          }}
+        ></GoogleMap>
+      </LoadScript>
+      <Button
+        onClick={(e) => setLocation(randomCoordinates())}
+      >
+        Click
+      </Button>
+    </>
   );
 };
 
