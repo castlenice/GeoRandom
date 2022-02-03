a5940acde17d7450ce3920a7581990c4feee6d0a
castlenice <sonja.schoenburg@gmail.com>
2022-02-03 15:15:20 +0100

    display coordinates object

--- src/components/Game.js
+++ src/components/Game.js
@@ -20,10 +20,10 @@ const containerStyle = {
 
 const randomCoordinates = () => {
   const P = {
-    latitude: 51.453825,
-    longitude: 7.032503,
+    latitude: 50.11321, //mittelpunkt der europaeischen union
+    longitude: 9.253958,
   };
-  const R = 500000;
+  const R = 424000;
   const randomPoint = randomLocation.randomCirclePoint(P, R);
   return {
     lat: randomPoint.latitude,
@@ -31,32 +31,21 @@ const randomCoordinates = () => {
   };
 };
 
-
 const firstCoordinates = {
   lat: 51.453825,
   lng: 7.032503,
 };
 
-
-
 const Game = () => {
   const [location, setLocation] = useState(firstCoordinates);
   const [correctLocations, setCorrectLocations] = useState([]);
 
-  
-
   const currentLocation = {
     lat: location.lat,
-    lng: location.lng
-  }
-
+    lng: location.lng,
+  };
 
   console.log(correctLocations);
-
-
-  // useEffect(() => {
-  //   location.lat.length >= 5 && location.lat.push();
-  // }, [location.lat]);
 
   return (
     <div className="Game">
@@ -76,17 +65,32 @@ const Game = () => {
         ></GoogleMap>
       </LoadScript>
       <div className="button-1">
-        <Button disabled={correctLocations.length == 5} onClick={() => {
-          const nextLocation = randomCoordinates();
-          setLocation(nextLocation);
-          correctLocations.push(nextLocation)
-          setCorrectLocations(correctLocations)
-
-        }}>Play</Button>
-        <Button onClick={() => setLocation(currentLocation)}>Back to Location</Button>
+        <Button
+          disabled={correctLocations.length == 5}
+          onClick={() => {
+            const nextLocation = randomCoordinates();
+            setLocation(nextLocation);
+            correctLocations.push(nextLocation);
+            setCorrectLocations(correctLocations);
+            // function setCorrectLocations({ lat, lng }) {
+            //   const latLng = 'Your ' + lat + lng + '.';
+            //   return latLng;
+            // }
+          }}
+        >
+          Play
+        </Button>
+        <Button onClick={() => setLocation(currentLocation)}>
+          Back to Location
+        </Button>
       </div>
-      <div>
-        <h2></h2>
+      <div className="random-coordinate-list">
+        {correctLocations.map((coordinate) => (
+          <ul>
+            <li>Latitude: {coordinate.lat}</li>
+            <li>Longitude: {coordinate.lng}</li>
+          </ul>
+        ))}
       </div>
     </div>
   );
